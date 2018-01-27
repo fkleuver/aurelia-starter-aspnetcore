fcefb9e12adf6f5add109306779e3cb337ae4dd1
Fred Kleuver <fred@avurad.nl>
2018-01-27 03:10:40 +0100

    Remove aurelia-testing from plugin config

--- src/plugins/aurelia/index.ts
+++ src/plugins/aurelia/index.ts
@@ -1,8 +1,6 @@
 import { FrameworkConfiguration } from "aurelia-framework";
 import * as logging from "./logging";
-import * as testing from "./testing";
 
 export function configure(fxconfig: FrameworkConfiguration): void {
     logging.configure(fxconfig);
-    testing.configure(fxconfig);
 }
--- src/plugins/aurelia/testing.ts
+++
@@ -1,8 +0,0 @@
-import { FrameworkConfiguration } from "aurelia-framework";
-import environment from "../../environment";
-
-export function configure(fxconfig: FrameworkConfiguration): void {
-  if (environment.testing) {
-    fxconfig.plugin("aurelia-testing");
-  }
-}
